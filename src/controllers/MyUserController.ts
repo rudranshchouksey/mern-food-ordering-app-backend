ccf35d3122b2a0914425890006b0abe2a38669e3
Rudransh Chouksey <choukseyrudransh22@gmail.com>
2024-09-18 01:49:16 +0530

    payment

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -16,22 +16,22 @@ const getCurrentUser = async (req: Request, res: Response) => {
 };
 
 const createCurrentUser = async (req: Request, res: Response) => {
-    try {
-        const { auth0Id } = req.body;
-        const existingUser = await User.findOne({ auth0Id });
-    
-        if (existingUser) {
-          return res.status(200).send();
-        }
-    
-        const newUser = new User(req.body);
-        await newUser.save();
-    
-        res.status(201).json(newUser.toObject());
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Error creating user" });
-      }
+  try {
+    const { auth0Id } = req.body;
+    const existingUser = await User.findOne({ auth0Id });
+
+    if (existingUser) {
+      return res.status(200).send();
+    }
+
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    res.status(201).json(newUser.toObject());
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error creating user" });
+  }
 };
 
 const updateCurrentUser = async (req: Request, res: Response) => {
@@ -58,7 +58,7 @@ const updateCurrentUser = async (req: Request, res: Response) => {
 };
 
 export default {
+  getCurrentUser,
   createCurrentUser,
   updateCurrentUser,
-  getCurrentUser,
 };
